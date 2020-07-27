48e08a512dc3b260cb2c2f76249dc18f6d427e69
sheetalrprasad <sheetalraniprasad@gmail.com>
2020-07-27 11:54:42 +0530

    Revert "Check"
    This reverts commit dc7fe0ece24d4457caa6f0708e6fe11c12eeebde.

--- ProTwo/ProTwo/settings.py
+++ ProTwo/ProTwo/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from ProTwo.secret import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -21,6 +20,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR,"templates")
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '*ew)b=z6-x1#!h$6@0r*71hc+l)ak^c8n0*dq__k=%am)f1aqj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
